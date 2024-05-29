2c99b5a34c0e65264a08e10c3a0e2bf10f448b06
santiago marino <santiago.marino.diaz@gmail.com>
2024-05-29 16:33:26 -0500

    ok

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+// webpack.config.js
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Obtener __dirname en un módulo ES
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  }
+};
