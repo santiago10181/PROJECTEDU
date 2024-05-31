7111ff41ec07328e9d1fe79523057f807d21d9a2
santiago marino <santiago.marino.diaz@gmail.com>
2024-05-31 06:42:29 -0500

    ok

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 import bodyParser from "body-parser";
+import { log } from "console";
 import express from "express";
 import {dirname} from "path"
 import { fileURLToPath } from "url";
 
+import fs from "fs"
+import {JSDOM} from "jsdom"
 // inicializacion de express
 const app = express();
 // avtivacion de middleware
@@ -10,12 +13,25 @@ const __dirname = dirname(fileURLToPath(import.meta.url))
 app.use(bodyParser.urlencoded({extended: true}))
 app.set('view engine', 'ejs');
 app.use(express.static('public'))
-//Pagina de inicio
+//Pagina de inicio, creamos las fechas y la info a ingresar en el calendar
+
 app.get('/',(req,res)=>{
-    res.render('index')
+    
+    const date = new Date()
+    // establece el año
+    const año = date.getFullYear()
+    // estable el mes
+    const meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio","Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];  
+    const mesAct = meses[date.getMonth()]
+    // obtener dia
+    const diasSem = ["Dom", "Lun", "Mar", "Mié","Jue","Vie","Sáb"];
+    const diaAct = diasSem[date.getDay()]
+    res.render('index',{meses:meses,diasSem:diasSem})
+
+    
 })
 
-
+// idclient = 6916670296-qse3pc6trqav3bi7vl34tb4il5vgau4d.apps.googleusercontent.com
 
 
 
