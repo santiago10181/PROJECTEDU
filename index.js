daea475fbacf3753b40a3b934796002b6c97e8b2
santiago marino <santiago.marino.diaz@gmail.com>
2024-06-06 14:22:34 -0500

    agrgado el .env

--- index.js
+++ index.js
@@ -53,15 +53,15 @@ app.post("/contact",async (req,res)=>{
     secure: true,
     auth: {
     //El User admin de la web qu da permisos como remitente
-    user: 'admisiones.eysa@gmail.com',
+    user: process.env.EMAILTOSEND,
     //  aplicación generada desde la configuración de seguridad de la cuenta de Google, en contraseñas de aplicaaciones
-    pass: 'uugd hizj jfdz nsmw'
+    pass: process.env.PASSWORD_APPWEB
   }
 });
     
     let message = {
   from: email,
-  to: 'admisiones.eysa@gmail.com',
+  to: process.env.EMAILTOSEND,
   subject: asunto + '-' + nombre + '-' + numero ,
   text: text,
   html: text
