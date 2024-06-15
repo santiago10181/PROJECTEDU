e49169567a4213e42d626a91ffe7673876af97bf
santiago marino <santiago.marino.diaz@gmail.com>
2024-06-14 19:31:31 -0500

    listo para subir

--- aulaVirtual/src/loginAdmin.jsx
+++ aulaVirtual/src/loginAdmin.jsx
@@ -15,11 +15,11 @@ const FormLabel = (props) =>{
 function FormLog() {
   return (
     <form>
-      <img class="mb-4" src="images/eysaLogo.jpg" alt="" width="72" height="57" />
-      <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+      <img class="mb-4" src="images/eysaLogo.jpg" alt="" width="80" height="75" />
+      <h1 class="h3 mb-3 fw-normal mt-5">Please sign in</h1>
       <FormLabel  type="email" id="emailInput" placeholder="name@etece.com" forFloating="floatingInput" content="Email"/>
       <FormLabel  type="password" id="passInput" placeholder="Password" forFloating="floatingPassword" content="Password"/>
-      <button class="btn btn-primary w-100 py-2 mt-4" type="submit">Sign in</button>
+      <button class="btn btn-success w-100 py-2 mt-4" type="submit">Sign in</button>
       <p class="mt-5 mb-3 text-body-secondary">© {d.getFullYear()}</p>
     </form>
   )
