f61b3a790c59f3bbcb664aac2f01f2ed7ad17044
santiago marino <santiago.marino.diaz@gmail.com>
2024-06-25 07:53:21 -0500

    ok

--- aulaVirtual/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import FormLog from './loginAdmin.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <FormLog />
-  </React.StrictMode>,
-)
