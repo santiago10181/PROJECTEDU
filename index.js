a3feb8899e9641c56084f9f9555d0e15d20f1353
santiago marino <santiago.marino.diaz@gmail.com>
2024-06-26 14:41:33 -0500

    merge los register

--- index.js
+++ index.js
@@ -2,73 +2,108 @@ import bodyParser from "body-parser";
 import { log } from "console";
 import path from "path";
 import express from "express";
-import { dirname } from "path";
+import {dirname} from "path"
 import { fileURLToPath } from "url";
-import nodemailer from "nodemailer";
-import dotenv from 'dotenv';
-dotenv.config();
+import nodemailer from "nodemailer"
+// import dotenv from 'dotenv';
+// dotenv.config();
 
-// Inicialización de Express
+// inicializacion de express
 const port = process.env.PORT || 3000;
 const app = express();
-
-// Middleware
-const __dirname = dirname(fileURLToPath(import.meta.url));
-app.use(bodyParser.urlencoded({ extended: true }));
+// avtivacion de middleware
+const __dirname = dirname(fileURLToPath(import.meta.url))
+app.use(bodyParser.urlencoded({extended: true}))
 app.set("views", __dirname + "/Staticapp/views");
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + "/Staticapp/public"));
-app.use(express.static(path.join(__dirname, 'aulaVirtual/dist')));
 
-// Página de inicio
-app.get('/', (req, res) => {
-  res.render('index');
-});
 
-app.get("/nosotros", (req, res) => {
-  res.render('nosotros');
-});
 
-app.get("/programas", (req, res) => {
-  res.render('programas');
-});
 
-app.get("/contact", (req, res) => {
-  res.render('form_email');
-});
+//Pagina de inicio, creamos las fechas y la info a ingresar en el calendar//
 
-app.post("/contact", async (req, res) => {
-  let { email, text, nombre, numero, asunto } = req.body;
-  console.log(req.body);
+app.get('/',(req,res)=>{
+    
+    res.render('index')
+    
+})
 
-  let transporter = nodemailer.createTransport({
+app.get("/nosotros",(req,res)=>{
+    res.render('nosotros')
+})
+
+app.get("/programas",(req,res)=>{
+    res.render('programas')
+})
+app.get("/contact",(req,res)=>{
+    res.render('form_email')
+})
+app.post("/contact",async (req,res)=>{
+    // cuerpo del contenido
+    let email = req.body.email
+    let text = req.body.text
+    let nombre = req.body.nombre
+    let numero = req.body.numero
+    let asunto = req.body.asunto
+    console.log(req.body);
+    // 
+    let transporter = nodemailer.createTransport({
     service: 'gmail',
     host: 'smtp.gmail.com',
     port: 465,
     secure: true,
     auth: {
-      user: 'admisiones.eysa@gmail.com',
-      pass: process.env.PASSWORD_APPWEB
-    }
-  });
-
-  let message = {
-    from: email,
-    to: process.env.EMAILTOSEND,
-    subject: `${asunto} - ${nombre} - ${numero}`,
-    text: text,
-    html: text
-  };
-
-  transporter.sendMail(message, function (error, info) {
-    if (error) {
-      console.log('Error al enviar correo electrónico:', error);
-    } else {
-      console.log('Correo electrónico enviado:', info.response);
-    }
-    res.render('form_email');
-  });
+    //El User admin de la web qu da permisos como remitente
+    user: 'admisiones.eysa@gmail.com',
+    //  aplicación generada desde la configuración de seguridad de la cuenta de Google, en contraseñas de aplicaaciones
+    pass: "process.env.PASSWORD_APPWEB"
+  }
 });
+    
+    let message = {
+  from: email,
+  to: "process.env.EMAILTOSEND",
+  subject: asunto + '-' + nombre + '-' + numero ,
+  text: text,
+  html: text
+};
+transporter.sendMail(message, function(error, info){
+  if (error) {
+    console.log('Error al enviar correo electrónico:', error);
+  } else {
+    console.log('Correo electrónico enviado:', info.response);
+  }
+  res.render('form_email')
+});
+
+})
+
+///////REGISTER ALUMNO MATRICULA////////////////////
+app.get("/matricula",(req,res)=>{
+  
+  res.render("matricula")
+
+})
+app.post("/matricula",(req,res)=>{
+
+  let info = req.body
+  console.log(info);
+  res.send('OK')
+
+})
+
+///////REGISTER PROFE HDV////////////////////
+app.get("/hdv",(req,res)=>{
+  
+  res.render("profHDV")
+
+})
+app.post("/hdv",(req,res)=>{
+
+  let info = req.body
+  console.log(info);
+  res.send('OK')
 
 })
 //////LOGIN/////
@@ -78,7 +113,10 @@ app.get('/login',(req,res)=>{
   res.sendFile(path.join(__dirname, '/aulaVirtual/dist', 'login.html'));
 })
 
-// Puerto a activar
+// Puerto a activar 
+
 app.listen(port, () => {
-  console.log('Express server initialized');
+
+
+    console.log('Express server initialized');
 });
