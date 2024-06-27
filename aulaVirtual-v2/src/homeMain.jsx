b1e286ee71e196d282111ff5494880289e0ee4e8
santiago marino <santiago.marino.diaz@gmail.com>
2024-06-27 18:15:57 -0500

    prueba home

---
+++ aulaVirtual-v2/src/homeMain.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Home from './components/home.jsx'
+
+
+ReactDOM.createRoot(document.getElementById('root2')).render(
+
+    <Home />
+
+)
--- aulaVirtual-v2/vite.config.js
+++ aulaVirtual-v2/vite.config.js
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, 'src/index.html'),
+        home: resolve(__dirname, 'src/home.html'),
+      },
+    },
+  },
+  server: {
+    open: '/src/home.html', // Abrir login.html al iniciar el servidor
+    watch: {
+      usePolling: true
+    }
+  },
+});
